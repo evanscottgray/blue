a59774964b01914aead5b6abfb846939a8312a39
Evan Gray <evanscottgray@gmail.com>
2015-03-23 11:04:54 -0500

    add react bootstrap and add some views

--- blue/static/js/page.js
+++ blue/static/js/page.js
@@ -1,3 +1,15 @@
+var DropdownButton = ReactBootstrap.DropdownButton;
+var MenuItem = ReactBootstrap.MenuItem;
+var Navbar = ReactBootstrap.Navbar;
+var Nav = ReactBootstrap.Nav;
+var NavItem = ReactBootstrap.NavItem;
+var Modal = ReactBootstrap.Modal;
+var ModalTrigger = ReactBootstrap.ModalTrigger;
+var Button = ReactBootstrap.Button;
+var Glyphicon = ReactBootstrap.Glyphicon;
+var Input = ReactBootstrap.Input;
+var Label = ReactBootstrap.Label;
+
 var AppContainer = React.createClass({
     render: function () {
         return (
@@ -94,7 +106,7 @@ var BootstrapPersonPanel = React.createClass({
             }
             if(val) {
                 return (
-                    <li>{key}:  {val}</li>        
+                    <li>{key}:  {val}</li>
                 );
             }
         });
@@ -107,7 +119,7 @@ var BootstrapPersonPanel = React.createClass({
         );
 
         return (
-          <div className={classes}> 
+          <div className={classes}>
               <div className="panel-heading"> {this.props.title} </div>
               <div className="panel-body">
                   <ul>
@@ -119,23 +131,197 @@ var BootstrapPersonPanel = React.createClass({
     }
 });
 
+var AddDeviceModal = React.createClass({
+  loadRemote: function () {
+     $.ajax({
+         url: this.props.url,
+         dataType: 'json',
+         success: function (data) {
+             this.setState({data: data[this.props.dataKey]});
+         }.bind(this),
+         error: function (xhr, status, err) {
+             console.error(this.props.url, status, err.toString());
+         }.bind(this)
+     });
+ },
+ getInitialState: function () {
+     return (
+     {
+         data: []
+     }
+     );
+ },
+componentDidMount: function () {
+     this.loadRemote();
+     setInterval(this.loadRemote, this.props.pollInterval);
+ },
+  render: function() {
+    return (
+      <div>
+        <Modal {...this.props} bsStyle="primary" title="Modal heading" animation closeButton={true}>
+          <div className="modal-body">
+            <h4>Text in a modal</h4>
+            <p>This is where we would see a list of devices....</p>
+            </div>
+          <div className="modal-footer">
+            <Button onClick={this.props.onRequestHide}>Close</Button>
+          </div>
+        </Modal>
+        </div>
+      );
+  }
+});
+
+
+var EditPersonsModal = React.createClass({
+  loadRemote: function () {
+     $.ajax({
+         url: this.props.url,
+         dataType: 'json',
+         success: function (data) {
+             this.setState({data: data[this.props.dataKey]});
+         }.bind(this),
+         error: function (xhr, status, err) {
+             console.error(this.props.url, status, err.toString());
+         }.bind(this)
+     });
+ },
+ getInitialState: function () {
+     return (
+     {
+         data: []
+     }
+     );
+ },
+componentDidMount: function () {
+     this.loadRemote();
+     setInterval(this.loadRemote, this.props.pollInterval);
+ },
+  render: function() {
+    return (
+      <div>
+        <Modal {...this.props} bsStyle="primary" title="Edit People" animation closeButton={true}>
+          <div className="modal-body">
+              <PersonBox data={this.state.data} />
+          </div>
+          <div className="modal-footer">
+            <Button onClick={this.props.onRequestHide}>Close</Button>
+          </div>
+
+        </Modal>
+        </div>
+      );
+  }
+});
+
+var SelectPersonForm = React.createClass({
+  nextStep: function (e) {
+    e.preventDefault()
+    var data = "userid blah"
+    this.props.saveUserId(data)
+    this.props.nextStep()
+  },
+  handleChange: function (e) {
+    console.log('YEY');
+    console.log(e);
+  },
+  render: function () {
+    var inputNodes = this.props.data.map(function (d, index) {
+      return(
+        <Input type="radio" label={d.name} name="person" value={d.id} onClick={this.handleChange}/>
+      );
+    });
+    return (
+      <div className="person-select">
+          <h4>Select a Person </h4>
+          <form>
+              {inputNodes}
+              <Button type="submit" className="pull-right" onClick={this.nextStep}>
+                  Select User.
+              </Button>
+          </form>
+      </div>
+    );
+  }
+});
+
+var PersonBox = React.createClass({
+  getInitialState: function() {
+    return (
+      {
+        step: 1
+      }
+    );
+  },
+  saveUserId: function (data) {
+    this.setState({
+      userId: data
+    })
+  },
+  nextStep: function() {
+    this.setState({
+      step: this.state.step + 1
+    });
+  },
+  previousStep: function() {
+    this.setState({
+      step: this.state.step - 1
+    });
+  },
+  render: function() {
+    var step;
+    switch(this.state.step) {
+      case 1:
+        return (
+          <SelectPersonForm {...this.props}
+                            nextStep={this.nextStep}
+                            saveUserId={this.saveUserId} />
+        );
+      case 2:
+        return (
+          <div>
+               {this.state.userId}
+               <Glyphicon glyph="star" />
+          </div>
+        );
+    }
+    return (
+      step
+    );
+  }
+});
+
 var BootstrapNav = React.createClass({
     render: function () {
         return (
-            <div className="nav navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <a className="navbar-brand">
-                          {this.props.brand}
-                        </a>
-                    </div>
-                </div>
-            </div>
+          <Navbar className="fluid" brand={this.props.brand}>
+              <Nav right>
+                  <DropdownButton eventKey={1} title="Settings">
+                      <ModalTrigger modal={<AddDeviceModal url='/api/devices/nearby' pollInterval={5000} dataKey='nearby_devices' />}>
+                          <MenuItem eventKey="2">
+                              <span>
+                                  Add a Device
+                                  <Glyphicon className="pull-right" glyph="plus"/>
+                              </span>
+                          </MenuItem>
+                      </ModalTrigger>
+                      <MenuItem divider />
+                      <ModalTrigger modal={<EditPersonsModal url='/api/persons' pollInterval={3000} dataKey='persons' />}>
+                          <MenuItem eventKey="3">
+                              <span>
+                                  Edit Users
+                                  <Glyphicon className="pull-right" glyph="wrench"/>
+                              </span>
+                          </MenuItem>
+                      </ModalTrigger>
+                  </DropdownButton>
+              </Nav>
+          </Navbar>
         );
     }
 });
 
-React.render(
-    <AppContainer />,
-    document.getElementById('content')
-);
+  React.render(
+      <AppContainer />,
+      document.getElementById('content')
+  );
